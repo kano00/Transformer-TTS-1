ef3fade3c87763fc52ffe48ca751e47daa0e077e
pigiipanku <pigiipanku@gmail.com>
2020-09-01 16:03:46 +0900

    decoderのembedのdimを10000に

--- network.py
+++ network.py
@@ -61,7 +61,7 @@ class MelDecoder(nn.Module):
         :param num_hidden: dimension of hidden
         """
         super(MelDecoder, self).__init__()
-        self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(1024, num_hidden, padding_idx=0),
+        self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(10000, num_hidden, padding_idx=0),
                                                     freeze=True)
         self.pos_dropout = nn.Dropout(p=0.1)
         self.alpha = nn.Parameter(t.ones(1))
